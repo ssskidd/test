650a79e724ea643b40da49cec25b546d8275ff9a
ssskidd <93138267+ssskidd@users.noreply.github.com>
2021-11-30 19:11:10 +0300

    Update Point.cs

--- Point/Point/Point.cs
+++ Point/Point/Point.cs
@@ -6,7 +6,7 @@ namespace Point
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("/////");
+            Console.WriteLine("sssssssssss");
         }
     }
 }
