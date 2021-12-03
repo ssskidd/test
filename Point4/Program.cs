92372a8d39d4032d18c31854ce924de7624731dd
ssskidd <dr1ammy@mail.ru>
2021-12-04 01:46:36 +0300

    Point4_New

---
+++ Point4/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+namespace Point3
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Drawing;
+
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point(int x, int y, char sym)
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+
+        }
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+
+    class Figure
+    {
+        protected List<Point> pList;
+
+        public void Drow()
+        {
+            foreach (Point p in pList)
+            {
+
+                p.Draw();
+
+            }
+        }
+        public bool IsHit(Figure figure)
+        {
+            foreach (var p in pList)
+            {
+                if (figure.IsHit(p))
+                    return true;
+            }
+            return false;
+        }
+
+        public bool IsHit(Point point)
+        {
+            foreach (var p in pList)
+            {
+                if (p.IsHit(point))
+                    return true;
+            }
+            return false;
+        }
+
+
+    }
+    class Walls
+    {
+        List<Figure> wallList;
+
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallList = new List<Figure>();
+
+           
+            HorizontalLine upLine = new HorizontalLine(0, mapWidth - 2, 0, '#');
+            HorizontalLine downLine = new HorizontalLine(0, mapWidth - 2, mapHeight - 1, '#');
+            VerticalLine leftLine = new VerticalLine(0, mapHeight - 1, 0, '#');
+            VerticalLine rightLine = new VerticalLine(0, mapHeight - 1, mapWidth - 2, '#');
+
+            wallList.Add(upLine);
+            wallList.Add(downLine);
+            wallList.Add(leftLine);
+            wallList.Add(rightLine);
+        }
+
+        public bool IsHit(Figure figure)
+        {
+            foreach (var wall in wallList)
+            {
+                if (wall.IsHit(figure))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Draw()
+        {
+            foreach (var wall in wallList)
+            {
+                wall.Drow();
+            }
+        }
+    }
+
+    class VerticalLine : Figure
+    {
+        public VerticalLine(int yUp, int yDown, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yUp; y <= yDown; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+    }
+    class HorizontalLine : Figure
+    {
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Walls wall = new Walls(50, 25);
+            wall.Draw();
+
+            Console.ReadKey();
+        }
+    }
+}
